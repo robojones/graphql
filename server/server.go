4661c020c5b2cea14390df94bea6c8a8b26a1274
Luca Steeb <contact@luca-steeb.com>
2019-02-11 01:01:24 +0100

    init

---
+++ server/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/handler"
+	"github.com/prisma/prisma-examples/go/graphql/prisma-client"
+)
+
+const defaultPort = "4000"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	client := prisma.New(&prisma.Options{
+		Endpoint: "http://localhost:4466/graphql/dev",
+		Secret:   "",
+	})
+
+	resolver := Resolver{
+		Prisma: client,
+	}
+
+	http.Handle("/", handler.Playground("GraphQL Playground", "/query"))
+	http.Handle("/query", handler.GraphQL(NewExecutableSchema(Config{Resolvers: &resolver})))
+
+	log.Printf("Server is running on http://localhost:%s", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
